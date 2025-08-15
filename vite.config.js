d3ab1d67d7c4902811d4d280ef579f10a2bc2cfa
Virendra Vikram Singh <153907622+virendra0817@users.noreply.github.com>
2025-08-15 21:04:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    allowedHosts: ['keeper-app-1-22qy.onrender.com']
+  }
 })
